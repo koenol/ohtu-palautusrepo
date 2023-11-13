caaf86b383bb1a57c527e192bed8f63be6e8ca06
Olli Koskinen <olli.s.koskinen@helsinki.fi>
2023-11-13 21:50:35 +0200

    exercise 10 - 11

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,23 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
+    response = requests.get(url).json()
+
+    print("JSON-muotoinen vastaus:")
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        if player.nationality == "FIN":
+            players.append(player)
+
+    print("Players from Finland")
+    sort_players = sorted(players, key=lambda x: x.goals + x.assists, reverse=True)
+    for player in sort_players:
+        print(player)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,12 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
-    
+        self.nationality = dict["nationality"]
+        self.team = dict["team"]
+        self.goals = dict["goals"]
+        self.assists = dict["assists"]
+
     def __str__(self):
-        return self.name
+        return (
+            f"Player: {self.name:20}{self.team:5}{self.goals:2} + {self.assists:2} = {self.goals + self.assists}"
+        )
